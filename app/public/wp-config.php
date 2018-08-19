d5ad04739f61abcffda9295fe8e137a6b3012c78
Jordan Dobney <jorddobbo@gmail.com>
2018-08-19 14:10:18 +0100

    Booking page updates

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -37,6 +37,10 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+if (strpos($_SERVER['SERVER_SOFTWARE'], 'Flywheel/') !== false) {
+	$_SERVER['SERVER_SOFTWARE'] = 'nginx/1.10.1';
+}
+
 /**
  * Authentication Unique Keys and Salts.
  *
--- app/public/wp-content/themes/bee/page-booking.php
+++ app/public/wp-content/themes/bee/page-booking.php
@@ -8,6 +8,80 @@
 		</div>
 	</div>
 
+	<section class="home-rates booking-rates">
+		<div class="home-rates__container container">
+			<p class="home-rates__paragraph">Most of the rooms can be adapted to suit a variety of therapies and we have flexible arrangements. So, if you are a counsellor/psychotherapist/ life coach this could work for you. One room has a sink and is well suited to art therapy.</p>
+			<div class="home-rates__info">
+				<div class="home-rates__hourly">
+					<div class="home-rates__info-indicator">1</div>
+					<p class="home-rates__info-sub-title">Hourly</p>
+					<h2 class="home-rates__info-title">£12.50</h2>
+				</div>
+				<div class="home-rates__daily">
+					<div class="home-rates__info-indicator">2</div>
+					<p class="home-rates__info-sub-title">Daily</p>
+					<h2 class="home-rates__info-title">£60</h2>
+				</div>
+				<div class="home-rates__times">
+					<div class="home-rates__info-indicator">3</div>
+					<div class="home-rates__times-inner">
+						<div class="home-rates__times-single">
+							<p class="home-rates__info-sub-title">9:00 - 12:00</p>
+							<h2 class="home-rates__info-title">£25</h2>
+						</div>
+						<div class="home-rates__times-single">
+							<p class="home-rates__info-sub-title">12:00 - 15:00</p>
+							<h2 class="home-rates__info-title">£25</h2>
+						</div>
+						<div class="home-rates__times-single">
+							<p class="home-rates__info-sub-title">17:00 - 20:00</p>
+							<h2 class="home-rates__info-title">£30</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="booking-tabs">
+		<div class="booking-tabs__container container">
+			<div class="booking-tabs__list">
+				<div class="booking-tabs__item booking-tabs__room-1 active">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-1.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .01</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-2">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-2.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .02</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-3">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-3.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .03</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-4">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-4.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .04</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-5">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-5.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .05</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-6">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-6.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .06</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-7">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-7.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .07</h3>
+				</div>
+				<div class="booking-tabs__item booking-tabs__room-8">
+					<div class="booking-tabs__image" style="background-image: url('<?= get_template_directory_uri(); ?>/assets/img/home/room-8.jpg');"></div>
+					<h3 class="booking-tabs__title">Room .08</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="page-wrap__container container">
 
 		<?php while (have_posts()) : the_post(); ?>
